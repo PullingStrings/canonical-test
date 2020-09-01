ba8904445b68e335c620c580760a707a035c92e8
Tito Zwane <drammatik@gmail.com>
2020-09-01 09:48:08 +0100

    trying to get article to work

--- src/components/postList.js
+++ src/components/postList.js
@@ -14,31 +14,32 @@ class PostList extends Component {
       .then(data => {
         this.setState({
           posts: data
-        })
-      })
+        });
+      });
     }
 
       render() {
         let listposts = this.state.posts.map((post, index) => {
           return(
               <div className="col-4">
-              <div class="p-card--highlighted">
-                <div className="p-card" key={index}>
-                  <h2 className="p-card__title">CLOUD AND SERVER</h2>
-              <hr className="u-sv1" />
-                  <img src={post.featured_media} alt="title-cards" />
-                  <h3>{post.title.rendered}</h3>
-                  <p><i> By <a href={post._embedded.author[0].url}>
-                                {post._embedded.author[0].name}
-                        </a> <></>
-                   on {Moment(post.date).format('MMMM Do YYYY')}
-                   </i></p>
-              <hr className="u-sv1" />
-                <p>Article</p>
-                </div>
+                <div className="p-card--highlighted">
+                  <div className="p-card" key={index}>
+                    <h2 className="p-card__title">CLOUD AND SERVER</h2>
+                <hr className="u-sv1" />
+                    <img src={post.featured_media} alt="title-cards" />
+                      <h3>{post.title.rendered}</h3>
+                      <p><i> By
+                      <a href={post._embedded.author[0].url}>
+                          {post._embedded.author[0].name}
+                          </a> <></> on
+                          {Moment(post.date).format('D MMMM YYYY')}
+                          </i></p>
+                <hr className="u-sv1" />
+                    <p><a href={post._embedded["wp:term"][0].name}>Article</a></p>
+                  </div>
                 </div>
               </div>
-          )
+          );
         })
         return (
           <div className="container u-vertically-center">

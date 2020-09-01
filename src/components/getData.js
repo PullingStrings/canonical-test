04736851973c681004cc7f8d8452074c3a84b7e8
Tito Zwane <drammatik@gmail.com>
2020-09-01 09:01:11 +0100

    style and components

---
+++ src/components/getData.js
@@ -0,0 +1,5 @@
+// import React, {Components} from 'react';
+
+export const appUrl = "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json";
+
+export const getPosts = `${appUrl}`;
---
+++ src/components/postList.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react'
+import { getPosts } from './getData'
+import Moment from 'moment'
+
+class PostList extends Component {
+    state = {
+        posts: []
+    }
+
+    componentDidMount() {
+      let postUrl = `${getPosts}`;
+      fetch(postUrl)
+      .then(data => data.json())
+      .then(data => {
+        this.setState({
+          posts: data
+        })
+      })
+    }
+
+      render() {
+        let listposts = this.state.posts.map((post, index) => {
+          return(
+              <div className="col-4">
+              <div class="p-card--highlighted">
+                <div className="p-card" key={index}>
+                  <h2 className="p-card__title">CLOUD AND SERVER</h2>
+              <hr className="u-sv1" />
+                  <img src={post.featured_media} alt="title-cards" />
+                  <h3>{post.title.rendered}</h3>
+                  <p><i> By <a href={post._embedded.author[0].url}>
+                                {post._embedded.author[0].name}
+                        </a> <></>
+                   on {Moment(post.date).format('MMMM Do YYYY')}
+                   </i></p>
+              <hr className="u-sv1" />
+                <p>Article</p>
+                </div>
+                </div>
+              </div>
+          )
+        })
+        return (
+          <div className="container u-vertically-center">
+            <div className="row">
+              {listposts}
+            </div>
+          </div>
+        );
+      }
+    }
+
+    export default PostList;
